83042b68c73d68d26a994a8962c272b4d7243049
Hoangdevnull <pqhoang.dev@gmail.com>
2025-02-21 14:03:28 +0700

    fix: docs header

--- docs/src/config/site.ts
+++ docs/src/config/site.ts
@@ -5,9 +5,9 @@ export const APP_URL = 'https://decentrodev.github.io/polkadot-confidential-offc
 export const siteConfig = {
   name: 'Polkadot Confidential Offchain Worker - Documentation',
   metaTitle:
-    'Polkadot Confidential Offchain Worker is tool that simplifies the process of building asset transfer functionalities within the Polkadot ecosystem.',
+    'Polkadot Confidential Offchain Worker',
   description:
-    'Polkadot Confidential Offchain Worker is tool that simplifies the process of building asset transfer functionalities within the Polkadot ecosystem.',
+    'Polkadot Confidential Offchain Worker',
   ogImage: `${APP_URL}/og-image.jpg`,
   github: 'https://github.com/DecentroDev/polkadot-confidential-offchain-worker',
 };
