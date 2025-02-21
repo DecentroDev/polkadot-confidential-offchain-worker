0514b3793528bd62faedbf457c74ef14dc8e21cc
Hoangdevnull <pqhoang.dev@gmail.com>
2025-02-21 10:48:13 +0700

    feat: add docs website

---
+++ docs/src/app/(home)/page.tsx
@@ -0,0 +1,48 @@
+import { Icons } from "@/assets/icons";
+import { HStack } from "@/components/h-stack";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <section className="flex h-full flex-1 flex-col items-center justify-center">
+      <h1 className="text-center font-bold font-mono text-5xl">
+        Confidential API Off-Chain Worker
+      </h1>
+
+      <HStack pos="center" className="mt-6">
+        <Link href="/docs">
+          <Button>Documents</Button>
+        </Link>
+        <a
+          href="https://github.com/DecentroDev/polkadot-sufficient-assets"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Button variant="outline">
+            <Icons.github /> Github
+          </Button>
+        </a>
+      </HStack>
+
+      <div className="my-8 flex items-center gap-2 text-sm italic">
+        developed by
+      </div>
+
+      <a
+        href="https://x.com/PRMX_2024"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          src="https://decentrodev.github.io/polkadot-sufficient-assets/prmx.svg"
+          className="max-w-full"
+          width={150}
+        />
+      </a>
+    </section>
+  );
+};
+
+export default HomePage;
---
+++ docs/theme.config.tsx
@@ -0,0 +1,59 @@
+import { useRouter } from 'next/router';
+import type { DocsThemeConfig } from 'nextra-theme-docs';
+import React from 'react';
+import packageJson from './package.json' assert { type: 'json' };
+
+const config: DocsThemeConfig = {
+  logo: <span>Polkadot Sufficient Assets</span>,
+  project: {
+    link: 'https://github.com/DecentroDev/polkadot-confidential-offchain-worker.git',
+  },
+  docsRepositoryBase: 'https://github.com/DecentroDev/polkadot-confidential-offchain-worker.git/tree/main/docs',
+  feedback: {
+    content: null,
+  },
+  footer: {
+    text: <span className='text-sm'>{new Date().getFullYear()} © PRMX all rights reserved</span>,
+  },
+  sidebar: {
+    toggleButton: true,
+    defaultMenuCollapseLevel: 1,
+  },
+  search: {
+    placeholder: 'Search website...',
+  },
+  // banner: {
+  //   dismissible: true,
+  //   key: `version-${packageJson.version}`,
+  //   text: (
+  //     <a href='http://' target='_blank' rel='noopener noreferrer'>
+  //       Polkadot Sufficient asset is in public beta →
+  //     </a>
+  //   ),
+  // },
+  toc: {
+    float: true,
+    backToTop: true,
+  },
+  nextThemes: {
+    defaultTheme: 'dark',
+  },
+  useNextSeoProps() {
+    const { asPath, pathname, query } = useRouter();
+
+    if (['/', '/docs'].includes(asPath)) {
+      return { titleTemplate: 'Confidential API Off-Chain Worker' };
+    }
+
+    if (pathname.startsWith('/wrapped/')) {
+      return {
+        title: (query?.name ?? 'React app') + ' Wrapped | Confidential API Off-Chain Worker',
+      };
+    }
+
+    return { titleTemplate: `%s | Confidential API Off-Chain Worker` };
+  },
+  primarySaturation: 0,
+};
+
+export default config;
